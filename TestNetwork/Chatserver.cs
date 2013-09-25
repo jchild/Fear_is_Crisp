f2a0ac3186bff84aa14d2c06a3a6a3076c1d8046
Jonathan Child <jchild45@gmail.com>
2013-09-25 17:17:43 +0800

    Added TestNetwork Folder
    Testing Client Server. Basic server already implemented, need client.
    Server has basic chat functionality and skeleton code for sending player
    moves.

---
+++ TestNetwork/Chatserver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Chatserver
+{
+	 public Chatserver()
+        {
+	        //create our nickname and nickname by connection variables
+	        nickName = new Hashtable(100);
+	        nickNameByConnect = new Hashtable(100);
+	        //create our TCPListener object
+	        chatServer = new System.Net.Sockets.TcpListener(4296);
+	        //check to see if the server is running
+	        //while (true) do the commands
+	        while (true)
+	        {
+		        //start the chat server
+		        chatServer.Start();
+		        //check if there are any pending connection requests
+		        if (chatServer.Pending())
+		        {
+			         //if there are pending requests create a new connection
+			        Chat.Sockets.TcpClient chatConnection = chatServer.AcceptTcpClient();
+			        //display a message letting the user know they're connected
+			        Console.WriteLine("You are now connected");
+			        //create a new DoCommunicate Object
+			        DoCommunicate comm = new DoCommunicate(chatConnection);
+		        }
+	        }
+        }
+}
---
+++ TestNetwork/TestNetwork/ServerMain.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO;
+using Chat = System.Net;
+using System.Net;
+
+namespace TestNetwork
+{
+    class ServerMain
+    {
+        System.Net.Sockets.TcpListener server;
+        public static Hashtable userName;
+        public static Hashtable userNameByConnect;
+
+        public static void Main()
+        {
+            ServerMain server = new ServerMain();
+        }
+        public ServerMain()
+        {
+            userName = new Hashtable(100);
+            userNameByConnect = new Hashtable(100);
+            IPAddress serverIP = null;//need to find a way to change thig to server address
+            server = new System.Net.Sockets.TcpListener(serverIP, 800);
+
+            while (true)
+            {
+                server.Start();
+
+                if (server.Pending())
+                {
+                    Chat.Sockets.TcpClient connection = server.AcceptTcpClient();
+                    Console.WriteLine("You have connected");
+                }
+            }
+        }
+        //this broadcasts messages to all in server from a specific user
+        public static void BroadcastChatMsg(string usrNm, string mesg)
+        {
+            StreamWriter writer;
+            ArrayList ToRemove = new ArrayList(0);
+            Chat.Sockets.TcpClient[] tcpClient = new Chat.Sockets.TcpClient[ServerMain.userName.Count];
+            ServerMain.userName.Values.CopyTo(tcpClient, 0);
+
+            for (int i = 0; i < tcpClient.Length; i++)
+            {
+                try
+                {
+                    if (mesg.Trim() == "" || tcpClient[i] == null)
+                    {
+                        writer = new StreamWriter(tcpClient[i].GetStream());
+                        writer.WriteLine(usrNm + " : " + mesg);
+                        writer.Flush();
+                        writer = null;
+                    }
+                }
+                catch (Exception e44)
+                {
+                    string str = (string)ServerMain.userNameByConnect[tcpClient[i]];
+                    ServerMain.SystemMessage("*** " + str + " *** Has Disconnected");
+                    ServerMain.userName.Remove(str);
+                    ServerMain.userNameByConnect.Remove(tcpClient[i]);
+                }
+            }
+
+        }
+        //this is system messages
+        public static void SystemMessage(string msg)
+        {
+            StreamWriter writer;
+            ArrayList ToRemove = new ArrayList(0);
+
+            Chat.Sockets.TcpClient[] tcpClient = new Chat.Sockets.TcpClient[ServerMain.userName.Count];
+            ServerMain.userName.Values.CopyTo(tcpClient, 0);
+
+            for(int i = 0; i< tcpClient.Length; i ++){
+                try{
+                    if(msg.Trim() == "" || tcpClient[i] == null)
+                        continue;
+                    writer = new StreamWriter(tcpClient[i].GetStream());
+                    writer.WriteLine(msg);
+                    writer.Flush();
+                    writer = null;
+                }
+                catch(Exception e44){
+                    ServerMain.userName.Remove(ServerMain.userNameByConnect[tcpClient[i]]);
+                    ServerMain.userNameByConnect.Remove(tcpClient[i]);
+                }
+            }
+        }
+        public static void BroadcastCharMove(Player* player)
+        {
+            Chat.Sockets.TcpClient[] tcpClient = new Chat.Sockets.TcpClient[ServerMain.userName.Count];
+            ServerMain.userName.Values.CopyTo(tcpClient, 0);
+            
+            for (int i = 0; i < tcpClient.Length; i++)
+            { 
+                try
+                {
+                  //push packets to each connected user on the server
+                }
+                catch (Exception e44)
+                {
+                    ServerMain.userName.Remove(ServerMain.userNameByConnect[tcpClient[i]]);
+                    ServerMain.userNameByConnect.Remove(tcpClient[i]);
+                    continue;
+                }
+            }
+        }
+    }
+}
