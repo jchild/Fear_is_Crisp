07a846cbfc65dd49def33c71fc018ababde70407
Jonathan Child <jchild45@gmail.com>
2013-09-27 14:31:26 +0800

    Changed the Voxel game file back to Chris' copy

--- VoxelGame/VoxelGame/main.cpp
+++ VoxelGame/VoxelGame/main.cpp
@@ -1,80 +1,453 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "Block.h"
+#include "Camera.h"
+
 #ifdef __APPLE__
-#include <GLUT/glut.h>
+#include <GLUT/freeglut.h>
 #else
-#include <GL/glut.h>
+#include <GL/freeglut.h>
 #endif
 
-void renderScene(void) {
+float red = 1.0f, green = 0.0f, blue = 0.0f;
 
+//Vertex-Buffer Object
+GLuint vboId= 0;
+GLfloat* vertices;
+
+
+//DisplayStatistics displayUtil;
+
+// MENU DEFINITION
+#define RED 1
+#define GREEN 2
+#define BLUE 3
+#define ORANGE 4
+
+#define FILL 1
+#define LINE 2
+
+#define SHRINK 1
+#define NORMAL 2
+
+//Popup Menu Identifiers
+int fillMenu, shrinkMenu, mainMenu, colorMenu;
+
+//scale of snowman
+float scale = 1.0f;
+
+//Menu status
+int menuFlag = 0;
+
+//default font
+void *font = GLUT_BITMAP_TIMES_ROMAN_24;
+
+#define INT_GLUT_BITMAP_8_BY_13 1
+#define INT_GLUT_BITMAP_9_BY_15 2
+#define INT_GLUT_BITMAP_TIMES_ROMAN_10  3
+#define INT_GLUT_BITMAP_TIMES_ROMAN_24  4
+#define INT_GLUT_BITMAP_HELVETICA_10  5
+#define INT_GLUT_BITMAP_HELVETICA_12  6
+#define INT_GLUT_BITMAP_HELVETICA_18  7
+
+//Orthographics
+int width = 1024, height = 768;
+
+float blockSize = 0.25f;
+Block* m_pBlocks[32][32][32];
+
+/*TEST*/
+Camera camera;
+
+void drawSnowMan(){
+	glScalef(scale, scale, scale);
+	glColor3f(1.0f, 1.0f, 1.0f);
+
+	//Draw Body
+	glTranslatef(0.0f, 0.75f, 0.0f);
+	glutSolidSphere(0.75f, 20, 20);
+
+	//Draw Head
+	glTranslatef(0.0f, 1.0f, 0.0f);
+	glutSolidSphere(0.25f, 20, 20);
+
+	//Draw Eyes
+	glPushMatrix();
+	glColor3f(0.0f, 0.0f, 0.0f);
+	glTranslatef(0.05f, 0.10f, 0.18f);
+	glutSolidSphere(0.05f, 10, 10);
+	glTranslatef(-0.1f, 0.0f, 0.0f);
+	glutSolidSphere(0.05f, 10, 10);
+	glPopMatrix();
+
+	//Draw Nose
+	glColor3f(red, green, blue);
+	glRotatef(0.0f, 1.0f, 0.0f, 0.0f);
+	glutSolidCone(0.08f, 0.5f, 10, 2);
+}
+
+void renderCube()
+{	
+	glPushMatrix();
+		glBegin(GL_QUADS);
+			glColor3f(red, green, blue);
+			glVertex3f( blockSize, blockSize,-blockSize);          // Top Right Of The Quad (Top)
+			glVertex3f(-blockSize, blockSize,-blockSize);          // Top Left Of The Quad (Top)
+			glVertex3f(-blockSize, blockSize, blockSize);          // Bottom Left Of The Quad (Top)
+			glVertex3f( blockSize, blockSize, blockSize);          // Bottom Right Of The Quad (Top)
+
+			glVertex3f( blockSize,-blockSize, blockSize);          // Top Right Of The Quad (Bottom)
+			glVertex3f(-blockSize,-blockSize, blockSize);          // Top Left Of The Quad (Bottom)
+			glVertex3f(-blockSize,-blockSize,-blockSize);          // Bottom Left Of The Quad (Bottom)
+			glVertex3f( blockSize,-blockSize,-blockSize);          // Bottom Right Of The Quad (Bottom)
+
+			glVertex3f( blockSize, blockSize, blockSize);          // Top Right Of The Quad (Front)
+			glVertex3f(-blockSize, blockSize, blockSize);          // Top Left Of The Quad (Front)
+			glVertex3f(-blockSize,-blockSize, blockSize);          // Bottom Left Of The Quad (Front)
+			glVertex3f( blockSize,-blockSize, blockSize);          // Bottom Right Of The Quad (Front)
+
+			glVertex3f( blockSize,-blockSize,-blockSize);          // Bottom Left Of The Quad (Back)
+			glVertex3f(-blockSize,-blockSize,-blockSize);          // Bottom Right Of The Quad (Back)
+			glVertex3f(-blockSize, blockSize,-blockSize);          // Top Right Of The Quad (Back)
+			glVertex3f( blockSize, blockSize,-blockSize);          // Top Left Of The Quad (Back)
+
+			glVertex3f(-blockSize, blockSize, blockSize);          // Top Right Of The Quad (Left)
+			glVertex3f(-blockSize, blockSize,-blockSize);          // Top Left Of The Quad (Left)
+			glVertex3f(-blockSize,-blockSize,-blockSize);          // Bottom Left Of The Quad (Left)
+			glVertex3f(-blockSize,-blockSize, blockSize);          // Bottom Right Of The Quad (Left)
+
+			glVertex3f( blockSize, blockSize,-blockSize);          // Top Right Of The Quad (Right)
+			glVertex3f( blockSize, blockSize, blockSize);          // Top Left Of The Quad (Right)
+			glVertex3f( blockSize,-blockSize, blockSize);          // Bottom Left Of The Quad (Right)
+			glVertex3f( blockSize,-blockSize,-blockSize);          // Bottom Right Of The Quad (Right)
+		glEnd();
+	glPopMatrix();
+}
+
+
+void renderCubes()
+{
+	/*
+	for(int x = 0; x < 32; x++)
+	{
+		for(int y = 0; y < 32; y++)
+		{
+			for(int z = 0; z < 32; z++)
+			{
+				glTranslatef((GLfloat)x * blockSize, (GLfloat)y * blockSize, (GLfloat)z * blockSize);
+				renderCube();				
+			}
+		}
+	}*/
+	glTranslatef(0,1,0);
+	renderCube();
+}
+// ------------------------------------------
+//					TEXT
+// ------------------------------------------
+void renderBitmapString(float x, float y, float z, void *font, char *string){
+	char *c;
+	glRasterPos3f(x,y,z);
+	for(c = string; *c != '\0'; c++){
+		glutBitmapCharacter(font, *c);
+	}
+}
+
+void renderSpacedBitmapString(float x, float y, int spacing, void *font, char *string){
+	char *c;
+	float x1=x;
+
+	for(c = string; *c != '\0'; c++){
+		glRasterPos2f(x1, y);
+		glutBitmapCharacter(font, *c);
+		x1 = x1 + glutBitmapWidth(font, *c) + spacing;
+	}
+}
+
+void renderVerticalBitmapString(float x, float y, int bitmapHeight, void *font, char *string){
+	char *c;
+	int i;
+
+	for(c = string, i = 0; *c != '\0'; i++, c++){
+		glRasterPos2f(x, y+bitmapHeight * i);
+		glutBitmapCharacter(font, *c);
+	}
+}
+
+void renderStrokeFontString(float x, float y, float z, void *font, char *string){
+	char *c;
+	glPushMatrix();
+	
+	glTranslatef(x,y,z);
+
+	for(c = string; *c != '\0'; c++){
+		glutStrokeCharacter(GLUT_STROKE_ROMAN, *c);
+	}
+
+	glPopMatrix();
+}
+
+// ------------------------------------------
+//				KEYBOARD
+// ------------------------------------------
+void processNormalKeys(unsigned char key, int x, int y){
+	switch(key){
+	case 27:
+		glutDestroyMenu(mainMenu);
+		glutDestroyMenu(fillMenu);
+		glutDestroyMenu(colorMenu);
+		glutDestroyMenu(shrinkMenu);
+		exit(0);
+		break;
+	case 's':
+		if(!menuFlag)
+			glutChangeToSubMenu(2, "Shrink", shrinkMenu);
+		break;
+	case 'c':
+		if(!menuFlag)
+			glutChangeToSubMenu(2, "Color", colorMenu);
+		break;
+	case 'f':
+		if(!menuFlag)
+			glutChangeToSubMenu(3, "Fill", fillMenu);
+		break;
+	}
+	if(key == 27)
+		exit(0);	
+}
+
+// ------------------------------------------
+//					MENUS
+// ------------------------------------------
+void processMenuStatus(int status, int x, int y){
+	if(status == GLUT_MENU_IN_USE){
+		menuFlag = 1;
+	}
+	else{
+		menuFlag = 0;
+	}
+}
+
+void processMainMenu(int option){
+	//Nothing here
+	//All actions are for submenus
+}
+
+void processFillMenu(int option){
+	switch(option){
+		case FILL:
+			glPolygonMode(GL_FRONT,GL_FILL);
+			break;
+		case LINE:
+			glPolygonMode(GL_FRONT, GL_LINE);
+			break;
+	}
+}
+
+void processShrinkMenu(int option){
+	switch(option){
+		case SHRINK:
+			scale = 0.5f;
+			break;
+		case NORMAL:
+			scale = 1.0f;
+			break;
+	}
+}
+
+void processColorMenu(int option){
+	switch(option){
+		case RED :
+			red = 1.0f;
+			green = 0.0f;
+			blue = 0.0f;
+			break;	
+		case GREEN :
+			red = 0.0f;
+			green = 1.0f;
+			blue = 0.0f;
+			break;
+		case BLUE :
+			red = 0.0f;
+			green = 0.0f;
+			blue = 1.0f;
+			break;	
+		case ORANGE :
+			red = 1.0f;
+			green = 0.5f;
+			blue = 0.5f;
+			break;
+	}
+}
+
+void createPopupMenus(){
+	shrinkMenu = glutCreateMenu(processShrinkMenu);
+	glutAddMenuEntry("SHRINK", SHRINK);
+	glutAddMenuEntry("NORMAL", NORMAL);
+
+	fillMenu = glutCreateMenu(processFillMenu);
+	glutAddMenuEntry("FILL", FILL);
+	glutAddMenuEntry("Line", LINE);
+
+	colorMenu = glutCreateMenu(processColorMenu);
+	glutAddMenuEntry("RED", RED);
+	glutAddMenuEntry("GREEN", GREEN);
+	glutAddMenuEntry("BLUE", BLUE);
+	glutAddMenuEntry("ORANGE", ORANGE);
+
+	mainMenu = glutCreateMenu(processMainMenu);
+	glutAddSubMenu("Scale Mode", shrinkMenu);
+	glutAddSubMenu("Polygon Mode", fillMenu);
+	glutAddSubMenu("Color", colorMenu);
+	//attach the menu to the right button
+	glutAttachMenu(GLUT_RIGHT_BUTTON);
+	
+	//this will allow us to know if the menu is active
+	glutMenuStatusFunc(processMenuStatus);
+}
+
+void renderScene(void){
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	glBegin(GL_QUADS);
-		glVertex3f(-0.7f, -1.5f, -5.0f);
-		glVertex3f(0.7f, -1.5f, -5.0f);
-		glVertex3f(0.4f,-0.5f,-5.0f);
-		glVertex3f(-0.4f,-0.5f,-5.0f);
-	glEnd();
-	glBegin(GL_TRIANGLES); //Begin triangle coordinates
-    
-		//Pentagon
-		glVertex3f(0.5f, 0.5f, -5.0f);
-		glVertex3f(1.5f, 0.5f, -5.0f);
-		glVertex3f(0.5f, 1.0f, -5.0f);
-    
-		glVertex3f(0.5f, 1.0f, -5.0f);
-		glVertex3f(1.5f, 0.5f, -5.0f);
-		glVertex3f(1.5f, 1.0f, -5.0f);
-    
-		glVertex3f(0.5f, 1.0f, -5.0f);
-		glVertex3f(1.5f, 1.0f, -5.0f);
-		glVertex3f(1.0f, 1.5f, -5.0f);
-		//Triangle
-		glVertex3f(-0.5f, 0.5f, -5.0f);
-		glVertex3f(-1.0f, 1.5f, -5.0f);
-		glVertex3f(-1.5f, 0.5f, -5.0f);
-	glEnd(); //End triangle coordinates
 
-        glutSwapBuffers();
+	camera.UpdateCamera();
+		/*
+		if(deltaMove)
+			computePos(deltaMove);
+		if(deltaAngle)
+			computeDir(deltaAngle);
+
+		glLoadIdentity();
+		//Set Camera
+		gluLookAt(x, 1.0f, z,
+				  x + lx, 1.0f, z + lz,
+				  0.0f, 1.0f, 0.0f);	
+		*/
+
+	//Draw Ground
+	glColor3f(0.5f, 0.5f, 0.5f);
+	glBegin(GL_QUADS);
+		glVertex3f(-100.f, 0.0f, -100.0f);
+		glVertex3f(-100.f, 0.0f, 100.0f);
+		glVertex3f(100.f, 0.0f, 100.0f);
+		glVertex3f(100.f, 0.0f, -100.0f);
+	glEnd();
+
+	/*
+	char number[3];
+	for(int i = -3; i < 3; i++){
+		for(int j = -3; j < 3; j++){
+			glPushMatrix();
+			glTranslatef(i * 10.0f, 0, j * 10.0f);
+			drawSnowMan();
+			sprintf(number, "%d", (i+3)*6 + (j+3));
+			renderBitmapString(0.0f, 0.5f, 0.0f, (void *)font, number);
+
+			glPopMatrix();
+		}
+	}
+	*/
+
+	renderCubes();
+
+	/*
+	camera.changeProjection(false);
+
+	glColor3f(0.0f, 1.0f, 1.0f);
+
+	glPushMatrix();
+	glLoadIdentity();
+	renderSpacedBitmapString(5, 30, 5, GLUT_BITMAP_HELVETICA_18, displayUtil.get_Frames());	
+	glPopMatrix();	
+	
+	camera.changeProjection(true);	
+	*/
+
+	glutSwapBuffers();
 }
-void initRendering() {
-    //Makes 3D drawing work when something is in front of something else
-    glEnable(GL_DEPTH_TEST);
+
+/*
+void init(){
+	// register callbacks
+	glutDisplayFunc(renderScene);
+	glutReshapeFunc(changeSize);
+	glutIdleFunc(renderScene);
+
+	glutIgnoreKeyRepeat(1);
+	glutKeyboardFunc(processNormalKeys);
+	glutSpecialFunc(pressKey);
+	glutSpecialUpFunc(releaseKey);
+	glutMouseFunc(mouseButton);
+	glutMotionFunc(mouseMove);
+
+	// OpenGL init
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);	
+	displayUtil = DisplayStatistics();
+	
 }
-void handleKeypress(unsigned char key, //The key that was pressed
-                    int x, int y) {    //The current mouse coordinates
-    switch (key) {
-        case 27: //Escape key
-            exit(0); //Exit the program
-    }
+*/
+
+void changeSize(int w, int h)
+{
+	camera.changeSize(w,h);
 }
-void handleResize(int w, int h) {
-    //Tell OpenGL how to convert from coordinates to pixel values
-    glViewport(0, 0, w, h);
-    
-    glMatrixMode(GL_PROJECTION); //Switch to setting the camera perspective
-    
-    //Set the camera perspective
-    glLoadIdentity(); //Reset the camera
-    gluPerspective(45.0,                  //The camera angle
-                   (double)w / (double)h, //The width-to-height ratio
-                   1.0,                   //The near z clipping coordinate
-                   200.0);                //The far z clipping coordinate
+
+void mouseButton(int button, int state, int x, int y)
+{
+	camera.mouseButton(button, state, x, y);
+}
+
+void mouseMove(int x, int y)
+{
+	camera.mouseMove(x, y);
+}
+
+void pressKey(int key, int xx, int yy)
+{
+	camera.pressKey(key, xx, yy);
+}
+void releaseKey(int key, int x, int y)
+{
+	camera.releaseKey(key, x, y);
 }
 
 int main(int argc, char **argv) {
 
 	// init GLUT and create Window
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGB);
 	glutInitWindowPosition(100,100);
-	glutInitWindowSize(320,320);
-	glutCreateWindow("Tacos - GLUT");
-	initRendering();
+	glutInitWindowSize(1024,768);
+	glutCreateWindow("Lighthouse3D - GLUT Tutorial");
+	
+	camera = Camera();
+	
 	// register callbacks
 	glutDisplayFunc(renderScene);
-	glutKeyboardFunc(handleKeypress);
-    glutReshapeFunc(handleResize);
+
+	//Reshape window
+	glutReshapeFunc(changeSize);
+
+	//Called when application is idle
+	glutIdleFunc(renderScene);
+
+	//Keyboard
+	glutKeyboardFunc(processNormalKeys);
+	glutSpecialFunc(pressKey);
+
+	glutIgnoreKeyRepeat(1);
+	glutSpecialUpFunc(releaseKey);
+
+	//Mouse
+	glutMouseFunc(mouseButton);
+	glutMotionFunc(mouseMove);
+
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+
+	//init Menus
+	createPopupMenus();
 
 	// enter GLUT event processing cycle
 	glutMainLoop();
